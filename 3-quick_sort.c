dec16962fe97deb190ce7796189ae4bce0531456
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-06-20 01:33:19 +0000

    Update: 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -9,44 +9,50 @@
  */
 void quick_sort(int *array, size_t size)
 {
-        int left_limit, right_limit;
+	int left_limit, right_limit;
 
-        left_limit = 0;
-        right_limit = (int)(size - 1);
-        quick_sort_rec(array, left_limit, right_limit, size);
+	if (!array || size < 2)
+		return;
+	left_limit = 0;
+	right_limit = (int)(size - 1);
+	quick_sort_rec(array, left_limit, right_limit, size);
 }
 
 /**
  * quick_sort_rec - Recursive funtion to sort array
- * 
  * @array: Elements to be ordered
  * @left_limit: Left limit sort
  * @right_limit: Right limit sort
+ * @size: Array size
  */
 void quick_sort_rec(int *array, int left_limit, int right_limit, size_t size)
 {
-        int left, right, tmp, pivot;
+	long int p = array[right_limit], i = left_limit - 1, j, t;
 
-        left = left_limit;
-        right = right_limit;
-        pivot = array[(left + right) / 2];
-        do{
-                while(array[left] < pivot && left < right_limit)
-                        left++;
-                while(pivot < array[right] && right > left_limit)
-                        right--;
-                if (left <= right)
-                {
-                        tmp = array[left];
-                        array[left] = array[right];
-                        array[right] = tmp;
-                        left++;
-                        right--;
-                }
-        }while(left <= right);
-        print_array(array, size);
-        if (left_limit < right)
-                quick_sort_rec(array, left_limit, right, size);
-        if (right_limit > left)
-                quick_sort_rec(array, left, right_limit, size);
+	if (left_limit < right_limit)
+	{
+		for (j = left_limit; j < right_limit; j++)
+		{
+			if (array[j] <= p)
+			{
+				i++;
+				if (i != j)
+				{
+					t = array[i];
+					array[i] = array[j];
+					array[j] = t;
+					print_array(array, size);
+				}
+			}
+		}
+		if (i + 1 != right_limit)
+		{
+			t = array[i + 1];
+			array[i + 1] = array[right_limit];
+			array[right_limit] = t;
+			print_array(array, size);
+		}
+		quick_sort_rec(array, left_limit, i, size);
+		quick_sort_rec(array, i + 2, right_limit, size);
+	}
 }
